d54f09b4760e81c03a8e0ebb400890fcfd566063
Max <maximefremovse@gmail.com>
2026-04-07 14:20:38 -0500

    Update ratelimited to 0.2.1

--- Casks/ratelimited.rb
+++ Casks/ratelimited.rb
@@ -1,6 +1,6 @@
 cask "ratelimited" do
-  version "0.2.0"
-  sha256 "9ade95251654c1fdbf1808628f0f407bd88b0d8fb372477276afe2dc6c05d0c1"
+  version "0.2.1"
+  sha256 "45ce180f970ced2fe9c7a529d4d94387d765ae335c087c39d53714d21d2b7f77"
 
   url "https://github.com/max2697/RateLimited/releases/download/v#{version}/RateLimited.app.zip"
   name "RateLimited"
